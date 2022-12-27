46356be851382b8c97d9c29cafce4caf286906ec
Alexandros Maragkakis <alexm280470@gmail.com>
2022-12-27 21:29:13 +0200

    vuln #2b

--- vulnerabilities/csp/source/jsonp.php
+++ vulnerabilities/csp/source/jsonp.php
@@ -1,13 +1,6 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 
-if (array_key_exists ("callback", $_GET)) {
-	$callback = $_GET['callback'];
-} else {
-	return "";
-}
-
-$outp = array ("answer" => "15");
-
-echo $callback . "(".json_encode($outp).")";
+$outp = array("answer" => "15");
+echo json_encode($outp);
 ?>
